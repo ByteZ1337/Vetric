49e55ee2981554ead3f1d19fc25ee1137f7934e7
ByteZ1337 <0xbytez@gmail.com>
2021-11-24 23:38:54 +0100

    Delete old files

--- core/src/main/kotlin/xyz/xenondevs/vetric/jvm/InheritanceTree.kt
+++
@@ -1,6 +0,0 @@
-package xyz.xenondevs.vetric.jvm
-
-data class InheritanceTree internal constructor(val wrapper: ClassWrapper) {
-    val parentClasses = HashSet<String>()
-    val subClasses = HashSet<String>()
-}
--- core/src/main/kotlin/xyz/xenondevs/vetric/util/IOUtils.kt
+++
@@ -1,7 +0,0 @@
-package xyz.xenondevs.vetric.util
-
-import java.io.Flushable
-
-fun <T> T.flushClose() where T : Flushable, T : AutoCloseable {
-    flush(); close()
-}
