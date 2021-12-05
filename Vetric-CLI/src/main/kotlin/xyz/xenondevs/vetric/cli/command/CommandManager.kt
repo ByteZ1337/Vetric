984c31dcf2196e6448b06cbde4509f9c2745749e
ByteZ1337 <0xbytez@gmail.com>
2021-12-05 22:43:08 +0100

    Removed useless CountDownLatch

--- Vetric-CLI/src/main/kotlin/xyz/xenondevs/vetric/cli/command/CommandManager.kt
+++ Vetric-CLI/src/main/kotlin/xyz/xenondevs/vetric/cli/command/CommandManager.kt
@@ -6,7 +6,6 @@ import xyz.xenondevs.vetric.cli.command.impl.ExitCommand
 import xyz.xenondevs.vetric.cli.command.impl.HelpCommand
 import xyz.xenondevs.vetric.cli.terminal.Terminal
 import xyz.xenondevs.vetric.logging.warn
-import java.util.concurrent.CountDownLatch
 import kotlin.concurrent.thread
 
 object CommandManager {
@@ -16,24 +15,20 @@ object CommandManager {
     private var commandThread: Thread? = null
     
     fun startListening() {
-        val latch = CountDownLatch(1)
         commandThread = thread(name = "Command Handler", isDaemon = true) {
             while (!Vetric.exit) {
-                if (latch.count != 0L)
-                    latch.countDown()
                 handleCommand(Terminal.readLine().trim())
             }
             println("Goodbye")
             Terminal.close()
         }
-        latch.await()
     }
     
     private fun handleCommand(line: String) {
         val split = line.split(' ')
         val name = split[0]
         val args = split.drop(1)
-        val command = commands.find { it.name.equals(name, ignoreCase = true)}
+        val command = commands.find { it.name.equals(name, ignoreCase = true) }
         if (command == null) {
             warn("Unknown command: $name. Type 'help' for help.")
             return
